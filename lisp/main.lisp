bb4ec4ccbe8ab7f62150a415cfe6cd0b63dd3848
Yokota Yuki <y2q-actionman@users.noreply.github.com>
2021-03-10 12:22:14 +0900

    Cleanup main

--- lisp/main.lisp
+++ lisp/main.lisp
@@ -2,25 +2,15 @@
 
 
 (defun print-waza-hirameki-result (waza-hirameki-result)
+  "For `print-result'."
   (loop for (waza prob from level unique-weapon) in waza-hirameki-result
      do (format t " ~A	~5,3F	(from ~A)	(level ~A)	~@[~A~]~%"
                 waza prob from level unique-weapon)))
 
-(defun print-waza-list (waza-list
-                        閃き済み技-list
-                        enemy-waza-level
-                        character-閃き可能-waza-list
-                        include-固有技)
-  (print-waza-hirameki-result
-   (calc-waza-hirameki-list waza-list
-                            閃き済み技-list
-                            enemy-waza-level
-                            character-閃き可能-waza-list
-                            include-固有技)))
-
 (defun print-result (enemy-waza-level character-name
                      &key (include-固有技 nil)
                        (additional-閃き済み技-list nil))
+  "For `print-all'."
   (let* ((閃き済み技-list (append additional-閃き済み技-list *閃き済み技-list*))
          (閃き-type-id (find-閃き-type-id-by-character-name character-name))
          (閃き可能-waza-list (find-閃き可能-waza-list-by-閃き-type-id 閃き-type-id)))
@@ -29,11 +19,13 @@
     (format t "~& 初期技一覧: ~{~A~^ ~}~%"
             (find-character-初期技-list character-name 閃き済み技-list))
     (loop for waza-type in +技種類-list+
+       as waza-list = (find-技種類-技一覧-alist waza-type)
        do (format t "~A~%" waza-type)
-         (print-waza-list (find-技種類-技一覧-alist waza-type)
-                          閃き済み技-list enemy-waza-level 閃き可能-waza-list include-固有技))))
+         (print-waza-hirameki-result
+          (calc-waza-hirameki-list waza-list 閃き済み技-list enemy-waza-level 閃き可能-waza-list include-固有技)))))
 
 (defun print-all (enemy member-list enemy-waza-level additional-閃き済み技-list)
+  "For romasaga2-user main function."
   (format t "~&敵 ~A Level ~D~2%" enemy enemy-waza-level)
   (loop for m in member-list
      do (print-result enemy-waza-level m
@@ -47,6 +39,7 @@
 ;;; HTML output
 
 (defun print-all-waza-name-for-html-datalist (&optional (stream *standard-output*))
+  "For making the frontend HTML parts."
   (format stream "<datalist id=\"all_waza_name\">~%")
   (dolist (waza-list (mapcar #'cdr +技種類-技一覧-alist+))
     (loop for waza in (rest waza-list)
@@ -55,6 +48,7 @@
   (format stream "~&</datalist>"))
 
 (defun print-all-waza-name-for-html-dojo (&optional (stream *standard-output*))
+  "For making the frontend HTML parts."
   (format stream "<fieldset open>~%")
   (format stream "<legend>技道場登録状況</legend>~%")
   (loop for (type-name . waza-list) in +技種類-技一覧-alist+
@@ -79,7 +73,8 @@
     "calc.lisp"
     ))
 
-(defun make-romasaga2-js (&optional (output-file-name "romasaga2.js"))
+(defun make-romasaga2-js (&key (output-file-name "romasaga2.js")
+                            ((:print-pretty ps:*ps-print-pretty*) ps:*ps-print-pretty*))
   (with-open-file (out output-file-name :direction :output :if-exists :supersede)
     (let ((ps:*parenscript-stream* out))
       (dolist (file *required-ps-files*)
