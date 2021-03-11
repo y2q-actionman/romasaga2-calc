f8723772cd02419afb781ecd1cb02efe11b4490b
Yokota Yuki <y2q-actionman@users.noreply.github.com>
2021-03-11 13:36:37 +0900

    Added save box skeleton

--- doc/romasaga2_front.js
+++ doc/romasaga2_front.js
@@ -165,3 +165,49 @@ function updateHiramekiTable(characterElem) {
     const outputTable = characterElem.querySelector('.hiramekiTableOutput table');
     outputTable.replaceWith(table);
 }
+
+//
+// Saving
+//
+
+// From https://developer.mozilla.org/ja/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+function initSaveElement(element) {
+    if (storageAvailable('localStorage')) {
+    	element.removeAttribute('disabled');
+    } else {
+	element.setAttribute('disabled', true);
+    }
+}
+
+function saveToLocalStorage() {
+    console.log("TODO");
+}
+
+function loadFromLocalStorage() {
+    console.log("TODO");
+}
